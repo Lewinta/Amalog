6ce37df0d0fe636975c1a57a81e809e09d65beeb
Torquez Script <ftoribio@torquezscript.com>
2021-05-16 04:14:25 +0000

    Improvements to the Website

---
+++ amalog/hook/jinja_methods.py
@@ -0,0 +1,22 @@
+def get_total_payment_request(doctype, name):
+
+    #reference_name = doctype.name
+
+    #paymentrequeststotal = frappe.db.sql("""
+    #    SELECT SUM
+    #        (grand_total) as Grand_Total
+    #    FROM `tabPayment Request`
+    #    WHERE docstatus = 2 AND reference_name = %(reference_name)s
+    #    ORDER BY posting_date, referenceName""", {'referenceName': reference_name}, as_dict=1)
+
+
+    
+    #balanceTotal = 0
+
+    #for prquest in paymentrequests:
+    #    grandTotal = prquest['grand_total']
+    #    balanceTotal = grandTotal + balanceTotal
+    
+    #return [paymentrequeststotal]
+
+    pass
